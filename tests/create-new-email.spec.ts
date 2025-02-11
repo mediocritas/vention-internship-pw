d483c56f68cc0d507a869e014e789c0c88ddee66
Olga Pushkina <archillas@mail.ru>
2025-02-11 04:36:45 +0300

    Switched locators to CSS if possible

--- tests/create-new-email.spec.ts
+++ tests/create-new-email.spec.ts
@@ -2,44 +2,43 @@ import { test, expect } from '@playwright/test';
 import { testSettings } from '../playwright.config';
 import { customDragTo } from './helpers';
 
-
 test.describe('mailfence tests (eng loc)', async () => {
 
   test.beforeEach('login', async ({ page }) => {
     await page.goto(testSettings.baseURL);
-    await page.locator('//button[@id="signin"]').click();
-    await page.locator('//input[@id="UserID"]').fill(testSettings.envVars.login);
-    await page.locator('//input[@id="Password"]').fill(testSettings.envVars.password);
-    await page.locator('//input[@value="Enter"]').click();
+    await page.locator('#signin').click();
+    await page.locator('#UserID').fill(testSettings.envVars.login);
+    await page.locator('#Password').fill(testSettings.envVars.password);
+    await page.locator('input[value="Enter"]').click();
   });
 
   test('create new email', async ({ page }) => {
-    await page.locator('//*[@id="nav-mail"]').click();
-    await page.locator('//*[@id="mailNewBtn"]').click();
-    await page.locator('//*[@id="mailTo"]/input').fill(testSettings.envVars.login + '@mailfence.com');
-    await page.locator('//*[@id="mailSubject"]').fill(testSettings.envVars.emailSubject);
+    await page.locator('#nav-mail').click();
+    await page.locator('#mailNewBtn').click();
+    await page.locator('#mailTo input').fill(testSettings.envVars.login + '@mailfence.com');
+    await page.locator('#mailSubject').fill(testSettings.envVars.emailSubject);
     const frame = page.frameLocator('iframe.editable');
-    await frame.locator('//*[@role="textbox"]').fill(testSettings.envVars.textForTest);
+    await frame.locator('[role="textbox"]').fill(testSettings.envVars.textForTest);
     await page.locator('//*[text()="Attachment"]').click();
     await page.locator('input[type="file"]').setInputFiles(testSettings.filePath);
     await page.locator(`//*[contains(text(), "${testSettings.envVars.fileName}")]`)
       .waitFor({ state: 'visible' });
-    await page.locator('//*[@id="mailSend"]').click();
+    await page.locator('#mailSend').click();
 
-    await page.locator('//*[@id="treeInbox"]').click();
+    await page.locator('#treeInbox').click();
 
     for (let i = 0; i < testSettings.maxRetries; i++) {
-      await page.locator('//*[@title="Refresh"]').click();
+      await page.locator('[title="Refresh"]').click();
 
       if (await page.locator(
-        `//*[contains(@class, "listSubject") and @title="${testSettings.envVars.emailSubject}"]`
+        `.listSubject[title="${testSettings.envVars.emailSubject}"]`
       ).waitFor({ state: 'attached', timeout: 1000 }).then(() => true).catch(() => false)) {
         break;
       }
     }
 
     await page.locator(
-      `//*[contains(@class, "listSubject") and @title="${testSettings.envVars.emailSubject}"]`
+      `.listSubject[title="${testSettings.envVars.emailSubject}"]`
     ).first().click();
 
     await page.locator(`//*[text()="${testSettings.envVars.fileName}"]`).hover();
@@ -48,6 +47,7 @@ test.describe('mailfence tests (eng loc)', async () => {
     ).click();
 
     await page.locator('//*[text()="Save in Documents"]').click();
+    await page.waitForLoadState('load');
     await page.locator('//*[text()="My documents"]')
       .waitFor({ state: 'attached' });
     await page.locator('//*[text()="My documents"]').click();
@@ -63,10 +63,10 @@ test.describe('mailfence tests (eng loc)', async () => {
       }
     }
 
-    await page.locator('//*[@id="nav-docs"]').click();
+    await page.locator('#nav-docs').click();
 
     const fileToDrag = page.locator(`//*[text()="${testSettings.envVars.fileName}"]`);
-    const trashDirectory = page.locator(`//*[@id="doc_tree_trash"]`);
+    const trashDirectory = page.locator('#doc_tree_trash');
     await page.waitForLoadState('load');
 
     await fileToDrag.waitFor({ state: 'visible' });
@@ -74,10 +74,8 @@ test.describe('mailfence tests (eng loc)', async () => {
 
     await customDragTo(fileToDrag, trashDirectory, page);
 
-    await page.locator('//*[@id="doc_tree_trash"]').click();
-    await expect(page.locator(
-      `//*[text()="${fileName}"]`
-    )).toBeVisible();
+    await page.locator('#doc_tree_trash').click();
+    await expect(fileToDrag).toBeVisible();
   });
 
 });
