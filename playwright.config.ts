867a5fa122712e0a86d8a4ecb281af865577176a
Olga Pushkina <archillas@mail.ru>
2025-02-12 12:49:38 +0300

    Config optimization

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,18 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const testSettings = {
-  envVars: {
-    login: process.env.LOGIN || 'defaultLogin',
-    password: process.env.PASSWORD || 'defaultPassword',
-    emailSubject: process.env.SUBJECT || 'Test Email',
-    fileName: process.env.FILE_NAME || 'Test fileName',
-    textForTest: process.env.TEXTBOX_TEXT || 'randomTextForTest',
-    maxRetries: process.env.MAX_RETRIES || 20,
-    baseURL: process.env.BASE_URL || 'https://mailfence.com/en/',
-    domain: process.env.DOMAIN || '@mailfence.com',
-  },
-};
 
 export default defineConfig({
 
@@ -26,8 +14,7 @@ export default defineConfig({
   reporter: 'html',
 
   use: {
-    trace: 'on-first-retry',
-    ...testSettings,
+    trace: 'on-first-retry'
   },
 
   projects: [
