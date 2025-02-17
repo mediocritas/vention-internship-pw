8b7d5e29f7753c511cbbe78072150e75a93b5e99
Olga Pushkina <archillas@mail.ru>
2025-02-17 09:07:41 +0300

    Tests recreated with pom

---
+++ tests/po-create-new-email.spec.ts
@@ -0,0 +1,63 @@
+import { test, expect } from '@playwright/test';
+import path from 'path';
+import { faker } from '@faker-js/faker';
+import { createFileCopy, createFilePath, deleteFile } from './temp-files-helper';
+import MainPage from '../src/po/pages/main-page';
+import MailPage from '../src/po/pages/mail-page';
+import DocumentsPage from '../src/po/pages/documents-page';
+import NewMailPage from '../src/po/pages/new-mail-page';
+
+const emailSubject = process.env.SUBJECT! + faker.string.alpha(5);
+
+const seedFilePath = path.resolve(process.env.FILE_NAME!);
+let tempFilePath: string;
+
+let mainPage : MainPage;
+let mailPage : MailPage;
+let newMailPage : NewMailPage;
+let docPage : DocumentsPage;
+
+test.describe('mailfence tests (eng loc)', async () => {
+
+  test.beforeEach('login', async ({ page }) => {
+    tempFilePath = await createFilePath(seedFilePath);
+    mainPage = new MainPage(page, process.env.BASE_URL!);
+    await mainPage.navigate();
+    await mainPage.getLogin(process.env.LOGIN!, process.env.PASSWORD!)
+  });
+
+  test('create new email', async ({ page }) => {
+    mailPage = new MailPage(page);
+    newMailPage = await mailPage.goToNewEmailPage();
+    const testFile = await createFileCopy(seedFilePath, tempFilePath);
+    await newMailPage.sendNewEmail(
+      process.env.LOGIN + process.env.DOMAIN!,
+      emailSubject,
+      process.env.TEXTBOX_TEXT!,
+      tempFilePath,
+      testFile
+    )
+
+    await mailPage.waitUntilNewEmailAppears(parseInt(process.env.MAX_RETRIES!, 10), emailSubject);
+    await mailPage.openEmail(emailSubject);
+
+    await mailPage.saveAttachmentInMyDocDir(testFile, parseInt(process.env.MAX_RETRIES!, 10));
+
+    docPage = await mailPage.goToDocPage();
+    await docPage.dragDocumentInTrashDirectory(testFile);
+    await docPage.treeMenu.goToTrashDirectory();
+    await docPage.waitForDocumentInTrashDirectory(testFile);
+    await expect(docPage.getDocumentByName(testFile)).toBeVisible();
+  });
+
+});
+
+test.afterEach(async ({ page, context }) => {
+  try {
+    await context.tracing.stop({ path: 'test-results/traces/trace.zip' });
+  } catch { }
+  await page.close();
+  if (test.info().errors.length === 0) {
+    await deleteFile(tempFilePath);
+  }
+});
