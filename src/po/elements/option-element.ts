9712ab5d04e6a2a277696759eb597f4ed9842d8f
Olga Pushkina <archillas@mail.ru>
2025-02-20 02:00:18 +0300

    Gitignore update

--- src/po/elements/option-element.ts
+++
@@ -1,8 +0,0 @@
-import test, { Locator } from "@playwright/test";
-import BaseElement from "./base-element";
-
-export default class OptionElement extends BaseElement {
-    constructor(locator: Locator, name?: string) {
-        super(locator, name)
-    }
-}
