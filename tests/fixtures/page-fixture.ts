6616bf324c5a8cf593d57efd57bd548e153932f3
Olga Pushkina <archillas@mail.ru>
2025-02-21 00:38:28 +0300

    Custom page fixture added

---
+++ tests/fixtures/page-fixture.ts
@@ -0,0 +1,16 @@
+import { test as base } from "@playwright/test"
+import { closePage, setPage } from "../../src/core/page-utils"
+
+
+export type TestOptions = {
+    testHooks: string
+}
+
+export const test = base.extend<TestOptions>({
+
+    testHooks: [async ({ page }, use) => {
+        setPage(page);
+        await use('');
+        await closePage();
+    }, { auto: true }]
+})
--- tests/po-create-new-email.spec.ts
+++ tests/po-create-new-email.spec.ts
@@ -1,17 +1,19 @@
-import { test, expect } from '@playwright/test';
+import {  expect } from '@playwright/test';
+import {test} from './fixtures/page-fixture'
 import path from 'path';
 import { faker } from '@faker-js/faker';
-import { createFileCopy, createFilePath, deleteFile } from './utils/temp-files-helper';
-import MainPage from '../src/po/pages/main-page';
-import MailPage from '../src/po/pages/mail-page';
-import DocumentsPage from '../src/po/pages/documents-page';
-import NewMailPage from '../src/po/pages/new-mail-page';
+import { createFileCopy, createFilePath, deleteFile } from '../src/utils/temp-files-helper';
+import MainPage from '../src/pageobject/pages/main-page';
+import MailPage from '../src/pageobject/pages/mail-page';
+import DocumentsPage from '../src/pageobject/pages/documents-page';
+import NewMailPage from '../src/pageobject/pages/new-mail-page';
 
 const emailSubject = process.env.SUBJECT! + faker.string.alpha(5);
 
 const seedFilePath = path.resolve('.artefacts/' + process.env.FILE_NAME!);
 const sourceFilePath = path.resolve(process.env.FILE_NAME!);
 let tempFilePath: string;
+let testFileName: string;
 
 let mainPage: MainPage;
 let mailPage: MailPage;
@@ -20,17 +22,17 @@ let docPage: DocumentsPage;
 
 test.describe('mailfence tests (eng loc)', async () => {
 
-  test.beforeEach('login', async ({ page }) => {
+  test.beforeEach('login', async ({  }) => {
     tempFilePath = await createFilePath(seedFilePath);
-    mainPage = new MainPage(page);
+    testFileName = await createFileCopy(sourceFilePath, tempFilePath);
+    mainPage = new MainPage();
     await mainPage.navigate();
     await mainPage.toLogin(process.env.LOGIN!, process.env.PASSWORD!)
   });
 
-  test('create new email', async ({ page }) => {
-    mailPage = new MailPage(page);
+  test('create new email', async ({ }) => {
+    mailPage = new MailPage();
     newMailPage = await mailPage.goToNewEmailPage();
-    const testFileName = await createFileCopy(sourceFilePath, tempFilePath);
     await newMailPage.sendNewEmail(
       {
         addressee: process.env.LOGIN + process.env.DOMAIN!,
@@ -51,16 +53,15 @@ test.describe('mailfence tests (eng loc)', async () => {
     await docPage.treeMenu().goToTrashDirectory();
     await docPage.waitForDocumentInTrashDirectory(testFileName);
     await expect(docPage.documentsList().documentButton(testFileName).locator,
-      'error: file not found in trash directory').toBeVisible();
+      `error: file ${testFileName} not found in trash directory`).toBeVisible();
   });
 
 });
 
-test.afterEach(async ({ page, context }) => {
+test.afterEach(async ({ context }) => {
   try {
     await context.tracing.stop({ path: 'test-results/traces/trace.zip' });
   } catch { }
-  await page.close();
   if (test.info().errors.length === 0) {
     await deleteFile(tempFilePath);
   }
