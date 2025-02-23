ce02c65e4da061469cbde5eb2c428a8e6daf0989
Olga Pushkina <archillas@mail.ru>
2025-02-24 02:00:47 +0300

    Initinal test simple copy for parallel start

---
+++ tests/po-create-new-email1.spec.ts
@@ -0,0 +1,60 @@
+import { expect } from '@playwright/test';
+import { test } from './fixtures/page-fixture'
+import path from 'path';
+import { faker } from '@faker-js/faker';
+import { createFileCopy, createFilePath, deleteFile } from '../src/utils/temp-files-helper';
+import MainPage from '../src/pageobject/pages/main-page';
+import MailPage from '../src/pageobject/pages/mail-page';
+import DocumentsPage from '../src/pageobject/pages/documents-page';
+import NewMailPage from '../src/pageobject/pages/new-mail-page';
+
+const emailSubject = process.env.SUBJECT! + faker.string.alpha(5);
+
+const seedFilePath = path.resolve('.artefacts/' + process.env.FILE_NAME!);
+const sourceFilePath = path.resolve(process.env.FILE_NAME!);
+let tempFilePath: string;
+let testFileName: string;
+
+test.describe('mailfence tests (eng loc)', async () => {
+
+  test.beforeEach('login', async ({ }) => {
+    tempFilePath = await createFilePath(seedFilePath);
+    testFileName = await createFileCopy(sourceFilePath, tempFilePath);
+  });
+
+  test('create new email', async ({ }) => {
+    await MailPage.navigate();
+    await MailPage.goToNewEmailPage();
+    await NewMailPage.sendNewEmail(
+      {
+        addressee: process.env.LOGIN + process.env.DOMAIN!,
+        emailSubject: emailSubject,
+        textMessage: faker.string.alpha(10),
+        filePath: tempFilePath,
+        fileName: testFileName
+      }
+    )
+
+    await MailPage.waitUntilNewEmailAppears(emailSubject);
+    await MailPage.openEmail(emailSubject);
+
+    await MailPage.saveAttachmentInMyDocDir(testFileName);
+
+    await MailPage.goToDocPage();
+    await DocumentsPage.dragDocumentInTrashDirectory(testFileName);
+    await DocumentsPage.treeMenu().goToTrashDirectory();
+    await DocumentsPage.waitForDocumentInDirectory(testFileName);
+    await expect(DocumentsPage.documentsList().documentButton(testFileName).locator,
+      `error: file ${testFileName} not found in trash directory`).toBeVisible();
+  });
+
+});
+
+test.afterEach(async ({ context }) => {
+  try {
+    await context.tracing.stop({ path: 'test-results/traces/trace.zip' });
+  } catch { }
+  if (test.info().errors.length === 0) {
+    await deleteFile(tempFilePath);
+  }
+});
