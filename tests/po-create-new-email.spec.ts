945304398bf2526495586ee051980a3c93830f13
Olga Pushkina <archillas@mail.ru>
2025-02-25 03:17:41 +0300

    Parallel launch within describe added

--- tests/po-create-new-email.spec.ts
+++ tests/po-create-new-email.spec.ts
@@ -1,25 +1,24 @@
 import { expect } from '@playwright/test';
-import { test } from './fixtures/page-fixture'
+import { test } from '../src/core/fixtures/page-fixture'
 import path from 'path';
 import { faker } from '@faker-js/faker';
-import { createFileCopy, createFilePath, deleteFile } from '../src/utils/temp-files-helper';
-import MainPage from '../src/pageobject/pages/main-page';
+import { createTextFile, deleteFile } from '../src/utils/temp-files-helper';
 import MailPage from '../src/pageobject/pages/mail-page';
 import DocumentsPage from '../src/pageobject/pages/documents-page';
 import NewMailPage from '../src/pageobject/pages/new-mail-page';
 
 const emailSubject = process.env.SUBJECT! + faker.string.alpha(5);
+const testFilePath = path.resolve('.artefacts/');
 
-const seedFilePath = path.resolve('.artefacts/' + process.env.FILE_NAME!);
-const sourceFilePath = path.resolve(process.env.FILE_NAME!);
 let tempFilePath: string;
-let testFileName: string;
+let tempFileName: string;
 
-test.describe('mailfence tests (eng loc)', async () => {
+test.describe.configure({ mode: 'parallel' });
 
   test.beforeEach('login', async ({ }) => {
-    tempFilePath = await createFilePath(seedFilePath);
-    testFileName = await createFileCopy(sourceFilePath, tempFilePath);
+    const result = await createTextFile(testFilePath);
+    tempFileName = result!.fileName;
+    tempFilePath = result!.filePath;
   });
 
   test('create new email', async ({ }) => {
@@ -31,24 +30,49 @@ test.describe('mailfence tests (eng loc)', async () => {
         emailSubject: emailSubject,
         textMessage: faker.string.alpha(10),
         filePath: tempFilePath,
-        fileName: testFileName
+        fileName: tempFileName
       }
     )
 
     await MailPage.waitUntilNewEmailAppears(emailSubject);
     await MailPage.openEmail(emailSubject);
 
-    await MailPage.saveAttachmentInMyDocDir(testFileName);
+    await MailPage.saveAttachmentInMyDocDir(tempFileName);
 
     await MailPage.goToDocPage();
-    await DocumentsPage.dragDocumentInTrashDirectory(testFileName);
+    await DocumentsPage.dragDocumentInTrashDirectory(tempFileName);
     await DocumentsPage.treeMenu().goToTrashDirectory();
-    await DocumentsPage.waitForDocumentInDirectory(testFileName);
-    await expect(DocumentsPage.documentsList().documentButton(testFileName).locator,
-      `error: file ${testFileName} not found in trash directory`).toBeVisible();
+    await DocumentsPage.waitForDocumentInDirectory(tempFileName);
+    await expect(DocumentsPage.documentsList().documentButton(tempFileName).locator,
+      `error: file ${tempFileName} not found in trash directory`).toBeVisible();
   });
 
-});
+  test('create new email copy', async ({ }) => {
+    await MailPage.navigate();
+    await MailPage.goToNewEmailPage();
+    await NewMailPage.sendNewEmail(
+      {
+        addressee: process.env.LOGIN + process.env.DOMAIN!,
+        emailSubject: emailSubject,
+        textMessage: faker.string.alpha(10),
+        filePath: tempFilePath,
+        fileName: tempFileName
+      }
+    )
+
+    await MailPage.waitUntilNewEmailAppears(emailSubject);
+    await MailPage.openEmail(emailSubject);
+
+    await MailPage.saveAttachmentInMyDocDir(tempFileName);
+
+    await MailPage.goToDocPage();
+    await DocumentsPage.dragDocumentInTrashDirectory(tempFileName);
+    await DocumentsPage.treeMenu().goToTrashDirectory();
+    await DocumentsPage.waitForDocumentInDirectory(tempFileName);
+    await expect(DocumentsPage.documentsList().documentButton(tempFileName).locator,
+      `error: file ${tempFileName} not found in trash directory`).toBeVisible();
+  });
+
 
 test.afterEach(async ({ context }) => {
   try {
