8ea633f60bfe9c01f9432680e300a7d19c78f44e
Olga Pushkina <archillas@mail.ru>
2025-02-25 03:19:24 +0300

    Test file creation optimized

--- src/utils/temp-files-helper.ts
+++ src/utils/temp-files-helper.ts
@@ -1,26 +1,15 @@
-import { join, parse } from 'path';
-import { unlink, copyFile } from 'fs/promises';
+import { join } from 'path';
+import { unlink, writeFile } from 'fs/promises';
 import { faker } from '@faker-js/faker';
 
-export const createFilePath = async (path: string): Promise<string> => {
+export const createTextFile = async (path: string) => {
     try {
-        const { dir, name, ext } = parse(path);
-        const fileName = `${name}_${faker.string.alpha(5)}${ext}`;
-        const filePath = join(dir, fileName);
-        return filePath;
+        const fileName = `testFile_${faker.string.alpha(5)}.txt`;
+        const filePath = join(path, fileName);
+        await writeFile(filePath, 'testFile');
+        return { fileName, filePath };
     } catch (error) {
-        console.error(`Incorrect path ${path}`);
-        throw error;
-    }
-}
-
-export const createFileCopy = async (sourcePath: string, targetPath: string): Promise<string> => {
-    try {
-        await copyFile(sourcePath, targetPath);
-        return parse(targetPath).base;
-    } catch (error) {
-        console.error(`Paths are incorrect`)
-        throw error;
+        console.error('')
     }
 }
 
