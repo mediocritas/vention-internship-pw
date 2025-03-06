c50c32a6d909d29621a351dfffb359cdd1a6afd4
Olga Pushkina <archillas@mail.ru>
2025-03-06 07:46:11 +0300

    New hooks introduced in fixtures

--- src/core/fixtures/page-fixture.ts
+++ src/core/fixtures/page-fixture.ts
@@ -1,16 +1,35 @@
 import { test as base } from "@playwright/test"
 import { closePage, setPage } from "../page-utils"
+import { setupEventListeners } from '../event-listener';
+import path from "path";
+import { createTextFile, deleteFile } from "../../utils/temp-files-helper";
 
 
 export type TestOptions = {
     testHooks: string
 }
 
-export const test = base.extend<TestOptions>({
+export type TestFile = {
+    fileName: string,
+    filePath: string
+}
 
-    testHooks: [async ({ page }, use) => {
+export const test = base.extend<TestOptions, { testFile: TestFile }>({
+
+    testFile: [async ({ }, use) => {
+        const testFilePath = path.resolve('.artefacts/');
+        const testFile = await createTextFile(testFilePath);
+        await use(testFile);
+    }, { scope: 'worker' }],
+
+    testHooks: [async ({ page, testFile }, use) => {
         setPage(page);
+        setupEventListeners(testFile);
         await use('');
+        if (test.info().errors.length === 0) {
+            await deleteFile(testFile!.filePath);
+        }
         await closePage();
-    }, { auto: true }]
+    }, { auto: true }],
+
 })
